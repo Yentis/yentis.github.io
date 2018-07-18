ed899e829d41acf7529c7d3f9689341055daa1fe
Yentis <yentl178@gmail.com>
2018-07-18 21:51:23 +0200

    update

--- emotescript.js
+++ emotescript.js
@@ -3,20 +3,32 @@ $(document).ready(function () {
         dataType: "json",
         url: "https://yentis.github.io/emotes/emotes.json",
         success: function (data) {
+            let list = document.getElementById('list');
+            let curElem;
             for (let key in data) {
                 if (data.hasOwnProperty(key)) {
                     let split = data[key].split('.');
+                    let newElem = document.createElement('div');
+                    newElem.id = split[0];
+                    newElem.style.float = 'left';
+                    newElem.style.margin = '2px';
+                    list.appendChild(newElem);
+                    curElem = newElem;
+
                     let imageUrl = "https://yentis.github.io/emotes/images/" + key + '.' + split[1];
                     let image = document.createElement('img');
                     let name = document.createElement('p');
-                    name.style.display = 'inline';
                     name.innerHTML = ' ' + split[0];
+                    name.style.textAlign = 'center';
+                    image.style.display = 'block';
+                    image.style.marginLeft = 'auto';
+                    image.style.marginRight = 'auto';
                     image.src = imageUrl;
-                    image.width = 100;
+                    image.height = 100;
 
-                    document.getElementById('list').appendChild(image);
-                    document.getElementById('list').appendChild(name);
-                    document.getElementById('list').appendChild(document.createElement('br'));
+                    curElem.appendChild(image);
+                    curElem.appendChild(name);
+                    curElem.appendChild(document.createElement('br'));
                 }
             }
         }
