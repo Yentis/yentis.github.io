0eefa759354cb05f43344d05b78699369d375767
Yentl Volcke <yentl178@gmail.com>
2019-01-16 05:40:44 +0100

    Update emotescript.js

--- emotescript.js
+++ emotescript.js
@@ -5,32 +5,39 @@ $(document).ready(function () {
         success: function (data) {
             let list = document.getElementById('list');
             let curElem;
-            for (let key in data) {
-                if (data.hasOwnProperty(key)) {
-                    let split = data[key].split('.');
-                    let newElem = document.createElement('div');
-                    newElem.id = split[0];
-                    newElem.style.float = 'left';
-                    newElem.style.margin = '2px';
-                    list.appendChild(newElem);
-                    curElem = newElem;
+            let newArray = [];
+            
+            data = Object.values(data);
+            data.forEach((emote, index) => {
+                newArray.push([index+1, emote]);
+            });
+            
+            newArray.reverse().forEach((emote) => {
+                let emoteID = emote[0],
+                    emoteName = emote[1];
+                let split = emoteName.split('.');
+                let newElem = document.createElement('div');
+                newElem.id = split[0];
+                newElem.style.float = 'left';
+                newElem.style.margin = '2px';
+                list.appendChild(newElem);
+                curElem = newElem;
 
-                    let imageUrl = "https://yentis.github.io/emotes/images/" + key + '.' + split[1];
-                    let image = document.createElement('img');
-                    let name = document.createElement('p');
-                    name.innerHTML = ' ' + split[0];
-                    name.style.textAlign = 'center';
-                    image.style.display = 'block';
-                    image.style.marginLeft = 'auto';
-                    image.style.marginRight = 'auto';
-                    image.src = imageUrl;
-                    image.height = 100;
+                let imageUrl = "https://yentis.github.io/emotes/images/" + emoteID+ '.' + split[1];
+                let image = document.createElement('img');
+                let name = document.createElement('p');
+                name.innerHTML = ' ' + split[0];
+                name.style.textAlign = 'center';
+                image.style.display = 'block';
+                image.style.marginLeft = 'auto';
+                image.style.marginRight = 'auto';
+                image.src = imageUrl;
+                image.height = 100;
 
-                    curElem.appendChild(image);
-                    curElem.appendChild(name);
-                    curElem.appendChild(document.createElement('br'));
-                }
-            }
+                curElem.appendChild(image);
+                curElem.appendChild(name);
+                curElem.appendChild(document.createElement('br'));
+            });
         }
     });
 });
