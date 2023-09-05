b7c4c07be902ac07912cdbc3950b0917c404f1b6
Freddy <freddyescobar@hotmail.fr>
2023-09-06 00:15:55 +0200

    fix: do not run code not supported by server side

--- ssg/src/services/platformService.ts
+++ ssg/src/services/platformService.ts
@@ -2,7 +2,7 @@ import { Platform } from 'quasar'
 import { Platform as PlatformType } from 'src/enums/platformEnum'
 
 export function getPlatform (): PlatformType {
-  if (Platform.is.cordova) return PlatformType.Cordova
-  if (Platform.is.electron) return PlatformType.Electron
+  if (Platform.is?.cordova) return PlatformType.Cordova
+  if (Platform.is?.electron) return PlatformType.Electron
   return PlatformType.Static
 }
