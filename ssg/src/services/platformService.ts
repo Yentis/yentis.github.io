1c1808eb019c1cc3da7f2a06e662c8496b7f3560
Yentis <yentl178@gmail.com>
2023-12-12 11:35:42 +0100

    update reader

--- ssg/src/services/platformService.ts
+++ ssg/src/services/platformService.ts
@@ -1,7 +1,7 @@
 import { Platform } from 'quasar'
 import { Platform as PlatformType } from 'src/enums/platformEnum'
 
-export function getPlatform (): PlatformType {
+export function getPlatform(): PlatformType {
   if (Platform.is?.cordova) return PlatformType.Cordova
   if (Platform.is?.electron) return PlatformType.Electron
   return PlatformType.Static
